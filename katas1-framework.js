807bb7370128664fc97d1023c4d4a045f37801c4
Ryan <jragard@gmail.com>
2018-10-28 10:00:47 -0400

    added file katas1-framework...this file is a skeleton for what we could give the students to use to complete their katas assignments, in order to make the assignments easier for coaches to grade.  The student could fill in the functions with pre-defined names within the module.exports object, and turn in the file.  I have also completed the katas assignment within this framework in katas1.js, and am importing this finished file in katas1test.js in order to test it.

---
+++ katas1-framework.js
@@ -0,0 +1,62 @@
+module.exports = {
+    oneThroughTwenty: function() {
+        // Return an array with the numbers from 1 to 20
+
+
+        return;
+    },
+    evensToTwenty: function() {
+        // Return an array with the even numbers from 1 to 20
+
+
+        return;
+    },
+    oddsToTwenty: function() {
+        // Return an array with the odd numbers from 1 - 20
+
+
+        return;
+    },
+    multiplesOfFive: function() {
+        // Return an array with the multiples of 5 up to 100
+
+
+        return;
+    },
+    squareNumbers: function() {
+        // Return an array with square numbers up to 100
+
+
+        return;
+    },
+    countingBackwards: function() {
+        // Return an array with the numbers counting backwards from 20 to 1
+
+
+        return;
+    },
+    evenNumbersBackwards: function() {
+        // Return an array with the even numbers counting backwards from 20
+
+
+        return;
+    },
+    oddNumbersBackwards: function() {
+        // Return an array with the odd numbers counting backwards from 20 to 1
+
+
+        return;
+    },
+    multiplesOfFiveBackwards: function() {
+        // Return an array with the multiples of five, counting backwards from 100
+
+
+        return;
+    },
+    squareNumbersBackwards: function() {
+        // Return an array with square numbers, counting backwards from 100
+        
+
+        return;
+    }
+ }
--- test/katas1test.js
+++ test/katas1test.js
@@ -1,27 +1,100 @@
 const katas1 = require('../katas1')
-var chai = require('chai')
-var assert = chai.assert;
+const chai = require('chai')
+const assert = chai.assert;
 
-let oneThroughTwenty = katas1.oneThroughTwenty();
-let evensToTwenty = katas1.evensToTwenty();
+const oneThroughTwenty = katas1.oneThroughTwenty();
+const evensToTwenty = katas1.evensToTwenty();
+const oddsToTwenty = katas1.oddsToTwenty();
+const multiplesOfFive = katas1.multiplesOfFive();
+const squareNumbers = katas1.squareNumbers();
+const countingBackwards = katas1.countingBackwards();
+const evenNumbersBackwards = katas1.evenNumbersBackwards();
+const oddNumbersBackwards = katas1.oddNumbersBackwards();
+const multiplesOfFiveBackwards = katas1.multiplesOfFiveBackwards();
+const squareNumbersBackwards = katas1.squareNumbersBackwards();
 
 
 
 describe('Display Numbers with For Loops', function() {
-    it('should have twenty elements', function() {
+
+    it('Kata 1  - should have twenty elements', function() {
         assert.equal(oneThroughTwenty.length, 20);
     })
-    it('should display 1 - 20', function() {
+    it('        - should display 1 - 20', function() {
         for (let counter = 1; counter < 21; counter++) {
             assert.isTrue(oneThroughTwenty.includes(counter))
         }
     })
-    it('should have 10 elements', function() {
+    it('Kata 2  - should have 10 elements', function() {
         assert.equal(evensToTwenty.length, 10)
     })
-    it('should display even numbers 2 - 20', function() {
+    it('        - should display even numbers 2 - 20', function() {
         for (let counter = 2; counter < 21; counter += 2) {
             assert.isTrue(evensToTwenty.includes(counter))
+        }
+    })
+    it('Kata 3  - should have 10 elements', function() {
+        assert.equal(oddsToTwenty.length, 10)
+    })
+    it('        - should display odd numbers 1 - 19', function() {
+        for (let counter = 1; counter < 20; counter += 2) {
+            assert.isTrue(oddsToTwenty.includes(counter))
+        }
+    })
+    it('Kata 4  - should have twenty elements', function() {
+        assert.equal(multiplesOfFive.length, 20);
+    })
+    it('        - should display multiples of 5 up to 100', function() {
+        for (let counter = 5; counter < 101; counter += 5) {
+            assert.isTrue(multiplesOfFive.includes(counter))
+        }
+    })
+    it('Kata 5  - should have 10 elements', function() {
+        assert.equal(squareNumbers.length, 10)
+    })
+    it('        - should display square numbers up to 100', function() {
+        for (let counter = 1; counter < 11; counter += 1) {
+            assert.isTrue(squareNumbers.includes(counter**2))
+        }
+    })
+    it('Kata 6  - should have twenty elements', function() {
+        assert.equal(countingBackwards.length, 20);
+    })
+    it('        - should display numbers counting backwards from 20', function() {
+        for (let counter = 20; counter > 0; counter -= 1) {
+            assert.isTrue(countingBackwards.includes(counter))
+        }
+    })
+    it('Kata 7  - should have 10 elements', function() {
+        assert.equal(evenNumbersBackwards.length, 10)
+    })
+    it('        - should display even numbers backwards from 20', function() {
+        for (let counter = 20; counter > 1; counter -= 2) {
+            assert.isTrue(evenNumbersBackwards.includes(counter))
+        }
+    })
+    it('Kata 8  - should have 10 elements', function() {
+        assert.equal(oddNumbersBackwards.length, 10)
+    })
+    it('        - should display odd numbers backwards from 20', function() {
+        for (let counter = 19; counter > 2; counter -= 2) {
+            assert.isTrue(oddNumbersBackwards.includes(counter))
+        }
+    })
+    it('Kata 9  - should have twenty elements', function() {
+        assert.equal(multiplesOfFiveBackwards.length, 20);
+    })
+    it('        - should display multiples of five counting backwards from 100', function() {
+        for (let counter = 100; counter > 1; counter -= 5) {
+            assert.isTrue(multiplesOfFiveBackwards.includes(counter))
+        }
+    })
+    it('Kata 10 - should have 10 elements', function() {
+        assert.equal(squareNumbersBackwards.length, 10)
+    })
+    it('        - should display square numbers counting backwards from 100', function() {
+        for (let counter = 10; counter > 0; counter -= 1) {
+            assert.isTrue(squareNumbersBackwards.includes(counter**2))
         }
     })
 });
