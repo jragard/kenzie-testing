a5be6d4baf4495a9ee73a03d3e3644fcb118db93
Travis Anderson <thetanderson@gmail.com>
2018-11-28 19:11:42 -0500

    made file structure, added dependencies

---
+++ Katas3/katas3.js
@@ -0,0 +1,31 @@
+const sampleArray = [
+  469,
+  755,
+  244,
+  245,
+  758,
+  450,
+  302,
+  20,
+  712,
+  71,
+  456,
+  21,
+  398,
+  339,
+  882,
+  848,
+  179,
+  535,
+  940,
+  472
+];
+
+// instructions need this to be in an html element,
+// not console log, will have to brainstorm on that
+function kata1() {
+  for (let i = 1; i <= 20; i++) {
+    console.log(i);
+  }
+}
+kata1();
