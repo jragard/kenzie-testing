e47fed4dcc01cf587cc75a7def3f9128d95aff02
thurt <taylor.a.hurt@gmail.com>
2018-12-02 20:38:55 -0500

    create initial skeleton files

---
+++ RollingDice/rollingDice-framework.js
@@ -0,0 +1,30 @@
+module.exports = {
+  rollDie: function() {
+    // Return a random integer from 1 to 6
+
+    return;
+  },
+  rollDice: function(numOfRolls) {
+    // Return an array whose length is numOfRolls, and each item is one rollDie() result
+
+    return;
+  },
+  createKeyCount: function(array) {
+    // Return an object whose keys represent every unique value in the input array
+    // and whose values represent the number of times that key was seen in the input array
+
+    return;
+  },
+  documentWriteObject: function(object) {
+    // Side-effecting function: should write the object's keys and values directly to the document
+    // in the format of
+    // key: value<br>
+
+    return;
+  },
+  createBarGraph: function(object) {
+    // Return a DOM subtree containing all the elements needed to render a bar graph from the input object data
+
+    return;
+  }
+};
---
+++ RollingDice/test/rollingDice-test.js
@@ -0,0 +1,11 @@
+const rollingDice = require("../rollingDice");
+const chai = require("chai");
+const assert = chai.assert;
+const FunctionAnalyzer = require("function-analyzer");
+
+describe("rollDie", () => {});
+
+describe("rollDice", () => {});
+describe("createKeyCount", () => {});
+describe("documentWriteObject", () => {});
+describe("createBarGraph", () => {});
