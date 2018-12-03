9c1d5403876e4995967ac0011169001de874d4c5
thurt <taylor.a.hurt@gmail.com>
2018-12-02 20:40:09 -0500

    create failing test for rollDie

--- RollingDice/test/rollingDice-test.js
+++ RollingDice/test/rollingDice-test.js
@@ -3,7 +3,15 @@ const chai = require("chai");
 const assert = chai.assert;
 const FunctionAnalyzer = require("function-analyzer");
 
-describe("rollDie", () => {});
+describe("rollDie", () => {
+  it("should use Math.floor(Math.random() * 6) + 1", () => {
+    assert(
+      new FunctionAnalyzer(rollingDice.rollDie).includes(
+        "Math.floor(Math.random() * 6) + 1"
+      )
+    );
+  });
+});
 
 describe("rollDice", () => {});
 describe("createKeyCount", () => {});
