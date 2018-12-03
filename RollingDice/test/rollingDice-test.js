1344c43632908f8db8f6e04128831a731f7182f0
thurt <taylor.a.hurt@gmail.com>
2018-12-02 22:44:40 -0500

    simplify times tests by factoring out randomNum

--- RollingDice/test/rollingDice-test.js
+++ RollingDice/test/rollingDice-test.js
@@ -18,23 +18,19 @@ describe("rollDie", () => {
 });
 
 describe("times", () => {
+  const randomNum = Math.floor(Math.random() * 100) + 1;
+
   it("should return an array with length equal to numOfTimes", () => {
-    // create a random number between 1 and 100
-    const numOfTimes = Math.floor(Math.random() * 100) + 1;
-    const result = times(rollDie, numOfTimes);
-    assert(result && result.length === numOfTimes);
+    const result = times(rollDie, randomNum);
+    assert(result && result.length === randomNum);
   });
   it("should call the fn numOfTimes", () => {
     const fnSpy = chai.spy();
-    const numOfTimes = Math.floor(Math.random() * 100) + 1;
-    const _ = times(fnSpy, numOfTimes);
-    fnSpy.should.have.been.called.exactly(numOfTimes);
+    const _ = times(fnSpy, randomNum);
+    fnSpy.should.have.been.called.exactly(randomNum);
   });
   it("should return an array whose values come the result of calling fn", () => {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
-
     const fnSpy = chai.spy(() => randomNum);
-
     const result = times(fnSpy, randomNum);
     assert(result.every(val => val === randomNum));
   });
