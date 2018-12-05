9e3c8073c25971614d1438086bb1325644e61d61
Ryan <jragard@gmail.com>
2018-12-05 10:47:36 -0500

    deleted unnecessary lines assigning 'assert' and 'should', which aren't used in this test file

--- Katas7/test/katas7test.js
+++ Katas7/test/katas7test.js
@@ -1,8 +1,6 @@
 const katas7 = require('../katas7')
 const chai = require('chai')
-const assert = chai.assert
 const expect = chai.expect
-const should = chai.should();
 
 const numbers = [1, 2, 3, 4, 5, 6]
 const oddNumbers = [1, 3, 5, 7, 9, 11]
