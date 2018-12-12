608426043254fb3ba5806b3e95ba590f7f8e3ee7
swkane <samuelwkane@gmail.com>
2018-12-12 09:48:07 -0500

    updated instructions for bonus and removed all TODO comments

--- Fizzbuzz/README.md
+++ Fizzbuzz/README.md
@@ -1,32 +1,22 @@
-# TODO
-
-- add more cases programmatically in the "string your function returns should contain the correct values"
-    update: Added more cases. Decide if this is enough
-- find a suitable answer to all questions in the _Lingering Questions_ section below
-
 # Instructions
 
-- Write a function named fizzbuzz.
-- Your function will have one parameter.
-- Your function should return a string with comma separated values with no spaces.
-- Your function should loop through 1 - the value of the parameter passed in.
+- Write a function named `fizzbuzz`.
+- This function will have one parameter - `maxValue`.
+- This function should return a string with comma separated values with no spaces.
+- This function should loop through 1 - the value of the parameter passed in (inclusive).
   - If a number is even, concatenate "Fizz," to the end of your string.
   - If a number is a multiple of 3, concatenate "Buzz," to the end of your string.
   - If a number is even and a multiple of 3 concatenate "FizzBuzz," to the end of your string.
   - If a number is none of these things, concatenate the number itself and a "," to the end of your string.
-- Your function should return this string after 1000 iterations of the loop.
-- Your last number should still have a comma after it.
+- This function should return this string after 1000 iterations of the loop.
+- If implemented correctly, the string you return should have a trailing comma, leave this comma
 
-# Lingering Questions
+- Write a second function named `writeToDOM`.
+- This function will have one parameter - `fizzbuzzResult`.
+- This function should take the string you pass to it, `fizzBuzzResult`, and attach it to the DOM
 
-- Do we want to them to then attach the results of their function to the DOM? // This would most likely use a string method which has not been introduced yet
-    update: I say yes and have included a function in the skeleton file.
-- How do we want to handle testing the bonus?
-    update: This is not answered but have made a test suite for the bonus. Have to decide what to do if they dont make it.
-- The current instructions will lead students to having a trailing comma at the end of their string, should we ask them to remove it or simply factor it into our tests?
-    update: I think trailing comma is fine and have included it as part of the result
-- Should we extend this so the parameter determines how many numbers into the sequence to go?
-    update: I have changed the flow to have a parameter. I included it in the skeleton file so they know to use it.
-- We could give them the following hint:
-  - The beginning of your string should resemble the following: "1,Fizz,Buzz,Fizz,5,FizzBuzz..."
-    update: Thought this was a good idea and have included it in the skeleton file.
+## Bonus
+
+- Write a third function named `fizzbuzzPrime`.
+- This function will follow all of the rules of your fizzbuzz function and 1 additional rule:
+  - If a number is a prime number, concatenate "Prime," to the end of your string
--- Fizzbuzz/test/fizzbuzz-test.js
+++ Fizzbuzz/test/fizzbuzz-test.js
@@ -14,7 +14,7 @@ const value10000 = 10000;
 describe("Fizzbuzz Test Suite", () => {
   describe("The string your function returns should follow this format: '1,Fizz,Buzz,Fizz,5,FizzBuzz...'", () => {
     it("should be a string", () => {
-      expect(typeof fb.fizzbuzz(value1000)).to.equal('string');
+      expect(typeof fb.fizzbuzz(value1000)).to.equal("string");
     });
     it("should contain no spaces", () => {
       expect(fb.fizzbuzz(value1000).includes(" ")).to.equal(false);
@@ -27,20 +27,21 @@ describe("Fizzbuzz Test Suite", () => {
     // solutionLength + 1 is due to trailing comma
     it("should iterate 1 time", () => {
       expect(fb.fizzbuzz(value1).split(",").length).to.equal(value1 + 1);
-    })
+    });
     it("should iterate 10 times", () => {
       expect(fb.fizzbuzz(value10).split(",").length).to.equal(value10 + 1);
-    })
+    });
     it("should iterate 1000 times", () => {
       expect(fb.fizzbuzz(value1000).split(",").length).to.equal(value1000 + 1);
     });
     it("should iterate 10000 times", () => {
-      expect(fb.fizzbuzz(value10000).split(",").length).to.equal(value10000 + 1);
-    })
-  })
+      expect(fb.fizzbuzz(value10000).split(",").length).to.equal(
+        value10000 + 1
+      );
+    });
+  });
   describe("The string your function returns should contain the correct values", () => {
     it("numbers that are not even or divisble by 3 should be added to the string", () => {
-      // TODO: add more cases programmatically to make more robust
       expect(fb.fizzbuzz(value10).split(",")[0]).to.equal("1");
       expect(fb.fizzbuzz(value10).split(",")[4]).to.equal("5");
       expect(fb.fizzbuzz(value100).split(",")[16]).to.equal("17");
@@ -48,35 +49,42 @@ describe("Fizzbuzz Test Suite", () => {
       expect(fb.fizzbuzz(value10000).split(",")[6000]).to.equal("6001");
     });
     it("even numbers that are not divisible by 3 should be replaced with 'Fizz'", () => {
-      // TODO: add more cases programmatically to make more robust
       expect(fb.fizzbuzz(value10).split(",")[1]).to.equal(evenAnswer);
       expect(fb.fizzbuzz(value100).split(",")[51]).to.equal(evenAnswer);
       expect(fb.fizzbuzz(value1000).split(",")[751]).to.equal(evenAnswer);
       expect(fb.fizzbuzz(value10000).split(",")[4501]).to.equal(evenAnswer);
     });
     it("numbers that are divisible by 3 but not even should be replaced with 'Buzz'", () => {
-      // TODO: add more cases programmatically to make more robust
       expect(fb.fizzbuzz(value10).split(",")[2]).to.equal(multipleOf3Answer);
       expect(fb.fizzbuzz(value100).split(",")[32]).to.equal(multipleOf3Answer);
-      expect(fb.fizzbuzz(value1000).split(",")[326]).to.equal(multipleOf3Answer);
-      expect(fb.fizzbuzz(value10000).split(",")[9998]).to.equal(multipleOf3Answer);
+      expect(fb.fizzbuzz(value1000).split(",")[326]).to.equal(
+        multipleOf3Answer
+      );
+      expect(fb.fizzbuzz(value10000).split(",")[9998]).to.equal(
+        multipleOf3Answer
+      );
     });
     it("even numbers that are divisible by 3 should be replaced with 'FizzBuzz'", () => {
-      // TODO: add more cases programmatically to make more robust
-      expect(fb.fizzbuzz(value10).split(",")[5]).to.equal(evenAndMultipleOf3Answer);
-      expect(fb.fizzbuzz(value100).split(",")[29]).to.equal(evenAndMultipleOf3Answer);
-      expect(fb.fizzbuzz(value1000).split(",")[905]).to.equal(evenAndMultipleOf3Answer);
-      expect(fb.fizzbuzz(value10000).split(",")[6665]).to.equal(evenAndMultipleOf3Answer);
+      expect(fb.fizzbuzz(value10).split(",")[5]).to.equal(
+        evenAndMultipleOf3Answer
+      );
+      expect(fb.fizzbuzz(value100).split(",")[29]).to.equal(
+        evenAndMultipleOf3Answer
+      );
+      expect(fb.fizzbuzz(value1000).split(",")[905]).to.equal(
+        evenAndMultipleOf3Answer
+      );
+      expect(fb.fizzbuzz(value10000).split(",")[6665]).to.equal(
+        evenAndMultipleOf3Answer
+      );
     });
   });
 });
 
-
-
 describe("FizzbuzzPrime Test Suite", () => {
   describe("The string your function returns should follow this format: '1,Fizz,Buzz,Fizz,Prime,FizzBuzz...'", () => {
     it("should be a string", () => {
-      expect(typeof fb.fizzbuzzPrime(value1000)).to.equal('string');
+      expect(typeof fb.fizzbuzzPrime(value1000)).to.equal("string");
     });
     it("should contain no spaces", () => {
       expect(fb.fizzbuzzPrime(value1000).includes(" ")).to.equal(false);
@@ -89,52 +97,69 @@ describe("FizzbuzzPrime Test Suite", () => {
     // solutionLength + 1 is due to trailing comma
     it("should iterate 1 time", () => {
       expect(fb.fizzbuzzPrime(value1).split(",").length).to.equal(value1 + 1);
-    })
+    });
     it("should iterate 10 times", () => {
       expect(fb.fizzbuzzPrime(value10).split(",").length).to.equal(value10 + 1);
-    })
+    });
     it("should iterate 1000 times", () => {
-      expect(fb.fizzbuzzPrime(value1000).split(",").length).to.equal(value1000 + 1);
+      expect(fb.fizzbuzzPrime(value1000).split(",").length).to.equal(
+        value1000 + 1
+      );
     });
     it("should iterate 10000 times", () => {
-      expect(fb.fizzbuzzPrime(value10000).split(",").length).to.equal(value10000 + 1);
-    })
-  })
+      expect(fb.fizzbuzzPrime(value10000).split(",").length).to.equal(
+        value10000 + 1
+      );
+    });
+  });
   describe("The string your function returns should contain the correct values", () => {
     it("numbers that are not even, not divisble by 3, or not prime should be added to the string", () => {
-      // TODO: add more cases programmatically to make more robust
       expect(fb.fizzbuzzPrime(value10).split(",")[0]).to.equal("1");
       expect(fb.fizzbuzzPrime(value100).split(",")[24]).to.equal("25");
       expect(fb.fizzbuzzPrime(value1000).split(",")[334]).to.equal("335");
       expect(fb.fizzbuzzPrime(value10000).split(",")[6000]).to.equal("6001");
     });
     it("numbers that are prime should be replace with 'Prime,'", () => {
-      // TODO: add more cases programmatically to make more robust
       expect(fb.fizzbuzzPrime(value10).split(",")[4]).to.equal("Prime");
       expect(fb.fizzbuzzPrime(value100).split(",")[16]).to.equal("Prime");
       expect(fb.fizzbuzzPrime(value1000).split(",")[330]).to.equal("Prime");
       expect(fb.fizzbuzzPrime(value10000).split(",")[2548]).to.equal("Prime");
     });
     it("even numbers that are not divisible by 3 should be replaced with 'Fizz'", () => {
-      // TODO: add more cases programmatically to make more robust
       expect(fb.fizzbuzzPrime(value10).split(",")[1]).to.equal(evenAnswer);
       expect(fb.fizzbuzzPrime(value100).split(",")[51]).to.equal(evenAnswer);
       expect(fb.fizzbuzzPrime(value1000).split(",")[751]).to.equal(evenAnswer);
-      expect(fb.fizzbuzzPrime(value10000).split(",")[4501]).to.equal(evenAnswer);
+      expect(fb.fizzbuzzPrime(value10000).split(",")[4501]).to.equal(
+        evenAnswer
+      );
     });
     it("numbers that are divisible by 3 but not even should be replaced with 'Buzz'", () => {
-      // TODO: add more cases programmatically to make more robust
-      expect(fb.fizzbuzzPrime(value10).split(",")[2]).to.equal(multipleOf3Answer);
-      expect(fb.fizzbuzzPrime(value100).split(",")[32]).to.equal(multipleOf3Answer);
-      expect(fb.fizzbuzzPrime(value1000).split(",")[326]).to.equal(multipleOf3Answer);
-      expect(fb.fizzbuzzPrime(value10000).split(",")[9998]).to.equal(multipleOf3Answer);
+      expect(fb.fizzbuzzPrime(value10).split(",")[2]).to.equal(
+        multipleOf3Answer
+      );
+      expect(fb.fizzbuzzPrime(value100).split(",")[32]).to.equal(
+        multipleOf3Answer
+      );
+      expect(fb.fizzbuzzPrime(value1000).split(",")[326]).to.equal(
+        multipleOf3Answer
+      );
+      expect(fb.fizzbuzzPrime(value10000).split(",")[9998]).to.equal(
+        multipleOf3Answer
+      );
     });
     it("even numbers that are divisible by 3 should be replaced with 'FizzBuzz'", () => {
-      // TODO: add more cases programmatically to make more robust
-      expect(fb.fizzbuzzPrime(value10).split(",")[5]).to.equal(evenAndMultipleOf3Answer);
-      expect(fb.fizzbuzzPrime(value100).split(",")[29]).to.equal(evenAndMultipleOf3Answer);
-      expect(fb.fizzbuzzPrime(value1000).split(",")[905]).to.equal(evenAndMultipleOf3Answer);
-      expect(fb.fizzbuzzPrime(value10000).split(",")[6665]).to.equal(evenAndMultipleOf3Answer);
+      expect(fb.fizzbuzzPrime(value10).split(",")[5]).to.equal(
+        evenAndMultipleOf3Answer
+      );
+      expect(fb.fizzbuzzPrime(value100).split(",")[29]).to.equal(
+        evenAndMultipleOf3Answer
+      );
+      expect(fb.fizzbuzzPrime(value1000).split(",")[905]).to.equal(
+        evenAndMultipleOf3Answer
+      );
+      expect(fb.fizzbuzzPrime(value10000).split(",")[6665]).to.equal(
+        evenAndMultipleOf3Answer
+      );
     });
   });
 });
