53c0f13b171b82b53e2a9d0aa4a73f7d35508b69
tgentry300 <taylor.gentry300@gmail.com>
2019-01-14 21:15:51 -0500

    successfully able to pull contents of student file relative to npm package location, to test its contents

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 const ArgumentParser = require('argparse').ArgumentParser;
+const path = require('path')
+const URI = require('urijs')
+
+
 
 const parser = new ArgumentParser({
     version: '1.0.0',
@@ -24,25 +28,71 @@ parser.addArgument(
     }
 )
 
+// function findStudentFile() {
+//     fs.readdirSync(startDir, (err, items) => {
+//         items.forEach(item => {
+//             if (caseInsensitivePattern.test(item)) {
+                
+               
+//             }
+//         });
+//     })
+
+// }
+
 const args = parser.parseArgs()
+// console.log(__dirname)
+// console.log(process.cwd())
+
+const startDir = process.cwd();
+// console.log(__filename)
 
 const caseInsensitivePattern = new RegExp(args.assessment, 'i')
+
 
 fs.readdir(`${__dirname}/Tests`, (err, items) => {
     items.forEach((item) => {
         if (caseInsensitivePattern.test(item)){
-
+            // console.log(process.cwd())
             process.chdir(`${__dirname}/Tests/${item}`)
+            const studentFileRelPath = (URI(startDir).relativeTo(`${__dirname}/Tests/${item}`))._string;
+            // console.log(studentFileRelPath)
+            
+            
+            
+            const arrayOfFiles = fs.readdirSync(startDir)
+
+            arrayOfFiles.forEach(file => {
+                if(caseInsensitivePattern.test(file)){
+                    let filename = file;
+                    let src = path.join(startDir, filename)
+                    console.log(src)
+                    let studentCode = fs.readFileSync(src, { encoding: "utf8" })
+
+                    console.log(studentCode + 'hey') 
+
+                }
+            })
+
+            
+
+            // console.log(findStudentFile())
+            
 
             // This installs all required npm modules and awaits the install before continuing to next exec call
             const installPackages = exec('npm i')
             installPackages.on('exit', () => {
                 process.exit
-                const { gitlink } = args
-                exec(`npm run test ${gitlink ? gitlink : ''}`, (error, stdout, stderr) => {
-                console.log(stdout)
             })
-            })
+
+            // 
+            // process.chdir(currentDirectory)
+            const { gitlink } = args
+            exec(`npm run test ${gitlink ? gitlink : ''}`, (error, stdout, stderr) => {
+            console.log(stdout)
+        })
         }
     })
 })
+
+
