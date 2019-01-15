ded747192187be57c135d9f32e9e0a56c8598ce7
tgentry300 <taylor.gentry300@gmail.com>
2019-01-14 23:52:00 -0500

    command line tool writes to separate temp file and passes text to run.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const { exec } = require('child_process');
+const {
+    exec
+} = require('child_process');
 const ArgumentParser = require('argparse').ArgumentParser;
 const path = require('path')
-const URI = require('urijs')
-
-
 
 const parser = new ArgumentParser({
     version: '1.0.0',
@@ -15,69 +14,48 @@ const parser = new ArgumentParser({
 })
 
 parser.addArgument(
-    ['assessment'],
-    {
+    ['assessment'], {
         help: 'assessment to be tested'
     }
 )
 
 parser.addArgument(
-    ['-g', '--gitlink'],
-    {
+    ['-g', '--gitlink'], {
         help: 'Github or Gitlab link to assessment file(s)'
     }
 )
 
-// function findStudentFile() {
-//     fs.readdirSync(startDir, (err, items) => {
-//         items.forEach(item => {
-//             if (caseInsensitivePattern.test(item)) {
-                
-               
-//             }
-//         });
-//     })
-
-// }
-
 const args = parser.parseArgs()
-// console.log(__dirname)
-// console.log(process.cwd())
 
 const startDir = process.cwd();
-// console.log(__filename)
 
 const caseInsensitivePattern = new RegExp(args.assessment, 'i')
 
-
 fs.readdir(`${__dirname}/Tests`, (err, items) => {
     items.forEach((item) => {
-        if (caseInsensitivePattern.test(item)){
-            // console.log(process.cwd())
-            process.chdir(`${__dirname}/Tests/${item}`)
-            const studentFileRelPath = (URI(startDir).relativeTo(`${__dirname}/Tests/${item}`))._string;
-            // console.log(studentFileRelPath)
+        if (caseInsensitivePattern.test(item)) {
             
+            process.chdir(`${__dirname}/Tests/${item}`)
             
             
             const arrayOfFiles = fs.readdirSync(startDir)
 
             arrayOfFiles.forEach(file => {
-                if(caseInsensitivePattern.test(file)){
+                if (caseInsensitivePattern.test(file)) {
                     let filename = file;
                     let src = path.join(startDir, filename)
                     console.log(src)
-                    let studentCode = fs.readFileSync(src, { encoding: "utf8" })
-
-                    console.log(studentCode + 'hey') 
-
+                    const studentCode = fs.readFileSync(src, {
+                        encoding: "utf8"
+                    })
+                    let tempFile = '../../temp.js'
+                    fs.appendFile(tempFile, studentCode, (err) => {
+                        if(err){
+                            console.log(err)
+                        }
+                    })
                 }
             })
-
-            
-
-            // console.log(findStudentFile())
-            
 
             // This installs all required npm modules and awaits the install before continuing to next exec call
             const installPackages = exec('npm i')
@@ -85,14 +63,16 @@ fs.readdir(`${__dirname}/Tests`, (err, items) => {
                 process.exit
             })
 
-            // 
-            // process.chdir(currentDirectory)
-            const { gitlink } = args
+            const {
+                gitlink
+            } = args
             exec(`npm run test ${gitlink ? gitlink : ''}`, (error, stdout, stderr) => {
-            console.log(stdout)
-        })
+                if(error){
+                    console.log(error)
+                }
+                console.log(stdout)
+                exec('rm ../../temp.js')
+            })
         }
     })
 })
-
-
