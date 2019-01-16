e923f6aa7fae45c4cdc88932d59343d8dca34a80
Ryan <jragard@gmail.com>
2019-01-15 23:22:11 -0500

    There was a PR to merge 'javascriptkatas3' branch into 'dev', but there were tons of merge conflicts due to the project structure being changed to accomodate for a command line tool to run tests.  Rather than resolve all the comments from the command line, I just copied the Katas3 directory from 'javascriptkatas3' branch onto my local machine, then switched to a new branch that was up to date with 'dev', and replaced the 'dev' Katas3 directory with the one I copied from the 'javascriptkatas3' branch, in order to keep the changes intact.

--- Tests/Katas3/katas3-framework.js
+++ Tests/Katas3/katas3-framework.js
@@ -22,7 +22,18 @@ const sampleArray = [
 ];
 
 function kata1() {
-  console.log("hi jake!");
+  // add your code here
+  return "Hello";
 }
 
-const kata2 = () => {};
+function kata2() {
+  // add your code here
+  return "World";
+}
+
+// for each kata make a function that follows the instructions
+
+// to run the tests to see your progress
+// in the command line, install dependencies by running: "npm install"
+// after installed, anytime you want to run the test run: "npm run test"
+module.exports = { kata1, kata2 };
