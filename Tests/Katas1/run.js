289d25c380feeb752b34369e4aa94d0d14dfdc0e
Ryan <jragard@gmail.com>
2019-02-09 21:33:43 -0500

    updated katas1 to work with cmd tool when testing local files, it should work for github/gitlab as well, but I still want to test it

--- Tests/Katas1/run.js
+++ Tests/Katas1/run.js
@@ -1,35 +1,53 @@
 const fs = require('fs');
 const axios = require('axios');
+const fetch = require('node-fetch');
 const { exec, spawn } = require('child_process');
 const { argv } = require('yargs');
 
 const tempFile = "test/s.js";
 const tempFileStream = fs.createWriteStream(tempFile);
 
+const arg = argv._
+
 if (argv._.length === 0) {
   defaultTest();
-} else if (argv._[0].includes("github")) {
+} else if (String(arg[0]).includes("github")) {
   const answer = /.*github.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(argv._[0]);
   const url = `https://raw.githubusercontent.com/${answer[1]}/${
     answer[2]
   }/master/katas1.js`;
   gitTest(url);
-} else if (argv._[0].includes("gitlab")) {
-  const url = argv._[0] + "/raw/master/katas1.js";
-  gitTest(url);
 } else {
-  console.log(`invalid command line parameter: ${argv._[0]}`);
+  const url = "https://gitlab.com/api/v4/projects/" + argv._[0] + "/repository/files/katas1%2Ejs?ref=master";
+  gitTest(url);
 }
 
 function defaultTest() {
-  studentCode = fs.readFileSync("../../temp.js", { encoding: "utf8" });
+  studentCode = fs.readFileSync("./test/temp.js", { encoding: "utf8" });
   runTests(studentCode);
 }
 
 function gitTest(url) {
+  if(url.includes("github")) {
   axios.get(url).then(response => {
     runTests(response.data);
   });
+} else {
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'PRIVATE-TOKEN': '5ZHEYQdoa5Tgx3yjpdP3'
+    }
+  })
+  .then(function(response) {
+    let res = response.body._readableState.buffer.head.data
+    let regex = /"content"/
+    let index = res.toString().search(regex)
+    let content = res.toString().slice(index + 11)
+    let decodedContent = Buffer.from(content, 'base64').toString();
+    runTests(decodedContent)
+  })
+}
 }
 
 function runTests(studentCode) {
@@ -42,5 +60,6 @@ function runTests(studentCode) {
       console.log(error);
     }
     exec(`rm ${tempFile}`);
+    exec(`rm ./test/temp.js`);
   });
 }
