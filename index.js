2bd5b5eb80ba8cefd12ba94acadec338322d1d84
Ryan <jragard@gmail.com>
2019-02-12 08:19:56 -0500

    some refactoring to make code cleaner and more readable

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const {
-    exec
-} = require('child_process');
+const { exec } = require('child_process');
 const ArgumentParser = require('argparse').ArgumentParser;
-const path = require('path')
+const path = require('path');
 
 const parser = new ArgumentParser({
     version: '1.0.0',
     addHelp: true,
     description: 'Kenzie Academy assessment testing tool'
-})
+});
 
 parser.addArgument(
     ['assessment'], {
         help: 'assessment to be tested'
     }
-)
+);
 
 parser.addArgument(
     ['-g', '--gitlink'], {
         help: 'Github or Gitlab link to assessment file(s)'
     }
-)
+);
 
-const args = parser.parseArgs()
+const args = parser.parseArgs();
+const userLocalDir = process.cwd();
+const argMatchesAssessment = new RegExp(args.assessment, 'i');
+const assessmentTests = `${__dirname}/Tests`
 
-const startDir = process.cwd();
+fs.readdir(assessmentTests, (err, testDirs) => {
+    testDirs.forEach((testDir) => {
+        let validTestDir = argMatchesAssessment.test(testDir);
 
-const caseInsensitivePattern = new RegExp(args.assessment, 'i')
+        if (validTestDir) {
+            let testLocation = `${assessmentTests}/${testDir}`
+            process.chdir(testLocation)
 
+            // Need to add logic to see if there are -g or -gitlink args.  If not, then go ahead and do what is below,
+            // reading the filenames in the user's cwd() and testing them.  If so, we can skip that part and just do the 
+            // run.js stuff. Currently, if the user supplies a -g or -gitlink arg signalling they want to grab the files 
+            // from a gitlab or github repo, and they also happen to be in the same directory as their local file, this 
+            // will grab the contents of their cwd() first and tends to mess up the script
 
-
-fs.readdir(`${__dirname}/Tests`, (err, items) => {
-    items.forEach((item) => {
-        if (caseInsensitivePattern.test(item)) {
+            const filesInUserLocalDir = fs.readdirSync(userLocalDir);
             
-            process.chdir(`${__dirname}/Tests/${item}`)
-            
-            const arrayOfFiles = fs.readdirSync(startDir)
-            
-            let studentFile
+            let userLocalFile;
 
-            arrayOfFiles.forEach(file => {
-                if (caseInsensitivePattern.test(file)) {
-                    studentFile = file
-                    let filename = file;
-                    let src = path.join(startDir, filename)
-                    const studentCode = fs.readFileSync(src, {
+            filesInUserLocalDir.forEach(file => {
+                let validLocalFile = argMatchesAssessment.test(file);
+
+                if (validLocalFile) {
+                    userLocalFile = file
+                    let pathToUserFile = path.join(userLocalDir, userLocalFile);
+
+                    const studentCode = fs.readFileSync(pathToUserFile, {
                         encoding: "utf8"
-                    })
+                    });
+
                     let tempFile = './test/temp.js'
                     let tempFileStream = fs.createWriteStream(tempFile);
 
@@ -58,16 +64,14 @@ fs.readdir(`${__dirname}/Tests`, (err, items) => {
                 }
                 if (file.includes(".html")) {
                     let htmlFile = file;
-                    let src = path.join(startDir, htmlFile)
-                    const htmlContent = fs.readFileSync(src, {
+                    let pathToHTMLFile = path.join(userLocalDir, htmlFile);
+
+                    const htmlContent = fs.readFileSync(pathToHTMLFile, {
                         encoding: "utf-8"
                     })
-                    // console.log(arrayOfFiles)
-                    // console.log(htmlContent)
+                    
+                    // For converting user HTML to a temp dom instance to test with jsdom, we might not use this soon
                     let oneLineHTML = htmlContent.replace(/\n|\t/g, '')
-
-                    // console.log(JSON.stringify(oneLineHTML))
-
                     let tempTXT = './test/temp.txt'
                     let tempTXTStream = fs.createWriteStream(tempTXT);
                     tempTXTStream.write(JSON.stringify(oneLineHTML));
@@ -83,7 +87,8 @@ fs.readdir(`${__dirname}/Tests`, (err, items) => {
                     if(error){
                         console.log(error)
                     }
-                    console.log(`Testing ${studentFile} in ${startDir}:`)
+                    // This code will console log this even if we are testing github/gitlab links, I believe
+                    console.log(`Testing ${userLocalFile} in ${userLocalDir}:`)
                     console.log(stdout)
                 })
             })
