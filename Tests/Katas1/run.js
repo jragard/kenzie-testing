d6fce163adf705117258784bea57799ca6540d82
Ryan <jragard@gmail.com>
2019-02-14 16:34:05 -0500

    cleanup

--- Tests/Katas1/run.js
+++ Tests/Katas1/run.js
@@ -8,7 +8,6 @@ const {
 const {
   argv
 } = require('yargs');
-// const { assessment } = require('../../index.js');
 
 const tempFile = "test/s.js";
 const tempFileStream = fs.createWriteStream(tempFile);
@@ -23,53 +22,6 @@ if (args == null) {
   const gitUser = argVars[1];
   const gitRepo = argVars[2];
 
-  // getSha returns a url which is used to fetch the filename
-  // let shaUrl = `https://api.github.com/repos/${gitUser}/${gitRepo}/commits`;
-
-  // const getSha = function() {
-
-  //   let promise = new Promise(function(resolve, reject) {
-
-  //     let result = fetch(shaUrl, {
-  //       method: 'GET',
-  //       headers: {
-  //         'PRIVATE-TOKEN': 'YiszMsh_vtySaoLLRZLd'
-  //       }
-  //     }).then(result => {
-  //       return result.json();
-  //     }).then(result => {
-  //       let filenameUrl = result[0].commit.tree.url;
-  //       return filenameUrl;
-  //     })
-  //     resolve(result);
-  //   });
-  //   return promise;
-  // }
-
-  // getSha().then(result => {
-  //   fetch(result, {
-  //     method: 'GET',
-  //       headers: {
-  //         'PRIVATE-TOKEN': 'YiszMsh_vtySaoLLRZLd'
-  //       }
-  //   }).then(result => {
-  //     return result.json()
-  //   }).then(result => {
-  //     let filename = result.tree[0].path;
-  //     let url = `https://raw.githubusercontent.com/${gitUser}/${gitRepo}/master/${filename}`;
-  //     console.log(url)
-  //   })
-  // })
-
-
-
-
-  // let fileToTest;
-  // let url;
-  // const argVars = /.*github.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(args);
-  // const gitUser = argVars[1];
-  // const gitRepo = argVars[2];
-
   const gitFetchUrl = `https://api.github.com/repos/${gitUser}/${gitRepo}/contents`;
 
   axios.get(gitFetchUrl).then(response => {
@@ -77,14 +29,12 @@ if (args == null) {
     url = `https://raw.githubusercontent.com/${gitUser}/${gitRepo}/master/${fileToTest}`;
     gitTest(url);
   });
-  
+
 } else {
 
   const argVars = /.*gitlab.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(args);
   const gitUser = argVars[1];
   const gitRepo = argVars[2];
-
-  const assessment = new RegExp(gitRepo, 'i');
 
   let getID = `https://gitlab.com/api/v4/projects/${gitUser}%2F${gitRepo}`
   const urlConstructor = {}
@@ -166,7 +116,6 @@ function gitTest(url) {
 }
 
 function runTests(studentCode) {
-  console.log('runTests')
   tempFileStream.write(studentCode.replace(/['"]?use strict['"]?/, ""));
   tempFileStream.write(
     "\nmodule.exports = { oneThroughTwenty: (typeof oneThroughTwenty) === 'function' && oneThroughTwenty, evensToTwenty: (typeof evensToTwenty) === 'function' && evensToTwenty, oddsToTwenty: (typeof oddsToTwenty) === 'function' && oddsToTwenty, multiplesOfFive: (typeof multiplesOfFive) === 'function' && multiplesOfFive, squareNumbers: (typeof squareNumbers) === 'function' && squareNumbers, countingBackwards: (typeof countingBackwards) === 'function' && countingBackwards, evenNumbersBackwards: (typeof evenNumbersBackwards) === 'function' && evenNumbersBackwards, oddNumbersBackwards: (typeof oddNumbersBackwards) === 'function' && oddNumbersBackwards, multiplesOfFiveBackwards: (typeof multiplesOfFiveBackwards) === 'function' && multiplesOfFiveBackwards, squareNumbersBackwards: (typeof squareNumbersBackwards) === 'function' && squareNumbersBackwards, }"
