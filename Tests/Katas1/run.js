c38ae4e6dae8b1da123bd1334da81c08112d85af
Unknown <keybladeschoosen1@gmail.com>
2019-02-19 20:52:13 -0500

    kata 1 prettified

--- Tests/Katas1/run.js
+++ Tests/Katas1/run.js
@@ -1,21 +1,23 @@
-const Test = require('../../testBase');
-const {exec, spawn} = require('child_process');
+const { TestBase } = require("../../testBase");
+const { exec, spawn } = require("child_process");
 const tempFileToTest = "./test/tempFileToTest.js";
-const fs = require('fs');
+const fs = require("fs");
 
-
-let test = new Test.TestBase(__dirname,
-    'oneThroughTwenty, evensToTwenty, oddsToTwenty, multiplesOfFive, squareNumbers, countingBackwards, evenNumbersBackwards, oddNumbersBackwards, multiplesOfFiveBackwards, squareNumbersBackwards'
+let test = new TestBase(
+  __dirname,
+  "oneThroughTwenty, evensToTwenty, oddsToTwenty, multiplesOfFive, squareNumbers, countingBackwards, evenNumbersBackwards, oddNumbersBackwards, multiplesOfFiveBackwards, squareNumbersBackwards"
 );
 
 function runTests(testBase) {
-  let js = testBase.getStudentFile.replace(/['"]?use strict['"]?/, "") + testBase.getFunctions;
-  fs.writeFileSync(tempFileToTest, js, function (err) {
+  let js =
+    testBase.getStudentFile.replace(/['"]?use strict['"]?/, "") +
+    testBase.getFunctions;
+  fs.writeFileSync(tempFileToTest, js, function(err) {
     console.log(err);
   });
-  spawn("../../node_modules/.bin/mocha", ['--colors'], {
+  spawn("../../node_modules/.bin/mocha", ["--colors"], {
     stdio: "inherit"
-  }).on("exit", function (error) {
+  }).on("exit", function(error) {
     if (error) {
       console.log(error);
     }
