0ede6f074656a5204ba3c8c846416a9064b4a1b6
Unknown <keybladeschoosen1@gmail.com>
2019-03-04 11:44:55 -0500

    Github Urls working

--- testBase.js
+++ testBase.js
@@ -8,6 +8,7 @@ const {argv} = require('./node_modules/yargs');
 /**
  * Builds the test documents
  *
+ * @class
  * @constructor
  * @param {string} directory - The path to the test directory.
  * @param {string} functions - CSV list of desired functions to test.
@@ -18,7 +19,6 @@ class TestBase {
     constructor(directory, functions, DOM = null, additional = null) {
         this.testDirectory = `${directory}/test/`;
         this.args = argv._[0];
-        this.studentFile = loadStudentFile(this.testDirectory, this.args);
         this.DOM = DOM;
         this.extra = additional;
         this.functions = loadFunctions(functions);
@@ -27,10 +27,11 @@ class TestBase {
     /**
      * Creates a tempFileToTest.js
      */
-    writeTestFile(){
+    async writeTestFile(){
         const tempFileToTest = `${this.testDirectory}/tempFileToTest.js`;
-        fs.writeFileSync(tempFileToTest, getTestFile(this.studentFile, this.functions, this.DOM, this.extra), function(err) {
-            console.log(err)});
+        let file = await loadStudentFile(this.testDirectory, this.args);
+        fs.writeFileSync(tempFileToTest, getTestFile(file, this.functions, this.DOM, this.extra), function(err) {
+           console.log(err)})
     }
 
     /**
@@ -41,53 +42,33 @@ class TestBase {
     }
 }
 
-
-
-
 module.exports = {
     TestBase
 };
 
-
-
-function getTestFile(file, functions, dom, extra){
-    let string = "";
-    if(dom !== null){
-        string += 'const jsdom = require("jsdom");\n' +
-            'const { JSDOM } = jsdom;\n' +
-            `const dom = new JSDOM(\"  ${dom}  \")\n` +
-            'global.document = dom.window.document;\n'
-    }
-    if(extra !== null){
-        string += " \n " + extra.join(" \n ");
-    }
-    string += file.replace(/['"]?use strict['"]?/, "");
-    string += functions;
-
-    return string;
-}
-
-function loadFunctions(functions){
-    let result = "\nmodule.exports = {";
-    functions.split(",").forEach(funct =>{
-        result += `${funct}: (typeof ${funct}) === 'function' && ${funct},`
-    });
-    result += "}";
-    return result;
-}
-
-
-function loadStudentFile(dir, args)
+async function loadStudentFile(dir, args)
 {
-    if (args == null) {
-        let r = loadLocalFile(dir);
-        exec(`rm ${dir}/temp.js`);
-        return r;
-    } else if (String(args).includes("github")) {
-        return loadGitFile(getGithubUrl(args));
-    } else {
-        return loadGitFile(getGitlabUrl(args))
+    let r;
+    switch (true) {
+        case (args == null):
+            r = loadLocalFile(dir);
+            exec(`rm ${dir}/temp.js`);
+            break;
+
+        case (args.includes("github")):
+           let url = await getGithubUrl(args);
+           console.log(url);
+           r = await loadGitFile(url);
+           break;
+
+        case (args.includes("gitlab")):
+            return loadGitFile(getGitlabUrl(args));
+
+        default:
+            console.log("Please input a valid Github, or Gitlab url");
     }
+    return r;
+
 }
 
 function loadLocalFile(dir){
@@ -101,12 +82,32 @@ function loadLocalFile(dir){
     }
 }
 
-function loadGitFile(){
-    console.log('Testing file from github repository - ' + url);
+async function getGithubUrl(args){
+    console.log("GetGithubUrl start");
+    const argVars = /.*github.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(args);
+    const gitUser = argVars[1];
+    const gitRepo = argVars[2];
+
+    const gitFetchUrl = `https://api.github.com/repos/${gitUser}/${gitRepo}/contents`;
+    let response = await axios.default.get(gitFetchUrl);
+    console.log("GetGithubURL inside Axios.default");
+
+    for(let i=0; i < response.data.length; i++){
+        let name = response.data[i].name;
+        console.log("getGitFileName " + name);
+        if(name.substring(name.length-2) === 'js'){
+            let url = `https://raw.githubusercontent.com/${gitUser}/${gitRepo}/master/${name}`
+            return url;
+        }
+    }
+}
+
+async function loadGitFile(url){
+    console.log("loadGitFile"+ 'Testing file from github repository - ' + url);
     if (url.includes("github")) {
-        axios.get(url).then(response => {
-            return response.data;
-        });
+        let file = await axios.default.get(url);
+        //console.log(file.data);
+        return file.data
     } else {
         console.log('Testing file from gitlab repository - ' + url);
         fetch(url, {
@@ -123,24 +124,8 @@ function loadGitFile(){
     }
 }
 
-function getGithubUrl(args){
-    const argVars = /.*github.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(args);
-    const gitUser = argVars[1];
-    const gitRepo = argVars[2];
 
-    const gitFetchUrl = `https://api.github.com/repos/${gitUser}/${gitRepo}/contents`;
-
-    axios.get(gitFetchUrl).then(response => {
-        for (let i = 0; i < response.data.length; i++) {
-            let name = response.data[i].name;
-            if (name.substring(name.length - 2) === 'js') {
-                return `https://raw.githubusercontent.com/${gitUser}/${gitRepo}/master/${name}`;
-            }
-        }
-    })
-}
-
-function getGitlabUrl(args){
+function getGitlabUrl(args) {
     const argVars = /.*gitlab.com\/([^/.]*)\/([^/.]*)[.git]?$/.exec(args);
     const gitUser = argVars[1];
     const gitRepo = argVars[2];
@@ -148,8 +133,8 @@ function getGitlabUrl(args){
     let getID = `https://gitlab.com/api/v4/projects/${gitUser}%2F${gitRepo}`
     const urlConstructor = {}
 
-    const getFinalUrl = function() {
-        let promise = new Promise(function(resolve, reject) {
+    const getFinalUrl = function () {
+        let promise = new Promise(function (resolve, reject) {
 
             let result = fetch(getID, {
                 method: 'GET',
@@ -166,7 +151,6 @@ function getGitlabUrl(args){
         });
         return promise
     };
-
     getFinalUrl().then(result => {
         let projectID = urlConstructor['project_id'];
         fetch(`https://gitlab.com/api/v4/projects/${projectID}/repository/tree`, {
@@ -190,4 +174,33 @@ function getGitlabUrl(args){
             return fileContentsUrl;
         })
     })
+}
+
+
+
+function getTestFile(file, functions, dom, extra){
+    //console.log("getTestFile" + file);
+    let string = "";
+    if(dom !== null){
+        string += 'const jsdom = require("jsdom");\n' +
+            'const { JSDOM } = jsdom;\n' +
+            `const dom = new JSDOM(\"  ${dom}  \")\n` +
+            'global.document = dom.window.document;\n'
+    }
+    if(extra !== null){
+        string += " \n " + extra.join(" \n ");
+    }
+    string += file.replace(/['"]?use strict['"]?/, "");
+    string += functions;
+
+    return string;
+}
+
+function loadFunctions(functions){
+    let result = "\nmodule.exports = {";
+    functions.split(",").forEach(funct =>{
+        result += `${funct}: (typeof ${funct}) === 'function' && ${funct},`
+    });
+    result += "}";
+    return result;
 }
