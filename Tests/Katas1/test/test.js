5756d507e5005b22beb5e71e265d4d8d593c0883
Unknown <keybladeschoosen1@gmail.com>
2019-03-05 09:44:37 -0500

    Katas1 Test Cleanup

--- Tests/Katas1/test/test.js
+++ Tests/Katas1/test/test.js
@@ -1,4 +1,4 @@
-const katas1 = require('./tempFileToTest.js')
+const katas1 = require('./tempFileToTest.js');
 const chai = require('../../../node_modules/chai');
 const expect = chai.expect;
 const colors = require('../../../node_modules/mocha/lib/reporters/base').colors;
@@ -28,18 +28,18 @@ describe('Kata 1, Display an array with 1 - 20', function() {
 
         it(studentAnswer.slice(0)[0] === 0 ? startsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(0)[0]).to.not.eql(0);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 21 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(21);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 19 ? endsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(19);
-        })    
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 2, Display an array with even numbers from 2 - 20', function() {
 
@@ -48,7 +48,7 @@ describe('Kata 2, Display an array with even numbers from 2 - 20', function() {
         if(typeof katas1.evensToTwenty != 'function') {
             this.skip();
         }
-    })
+    });
 
       if(typeof katas1.evensToTwenty === 'function') {
 
@@ -61,18 +61,18 @@ describe('Kata 2, Display an array with even numbers from 2 - 20', function() {
         
         it(studentAnswer.slice(0)[0] === 0 ? startsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(0)[0]).to.not.eql(0);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 1 ? endsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(18);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 2 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(22);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(katas1.evensToTwenty()).to.eql(correctAnswer)
         })
     }
-})
+});
 
 describe('Kata 3, Display an array with odd numbers 1 - 19', function() {
 
@@ -80,10 +80,10 @@ describe('Kata 3, Display an array with odd numbers 1 - 19', function() {
         if(typeof katas1.oddsToTwenty != 'function') {
             this.skip();
         }
-    })
+    });
 
-    const endsLate = 'Oops! The array includes the extra number 21...double check the end condition in your for loop'
-    const endsEarly = 'Oops! The array stops at 17 when it should include odd numbers up to 20...double check the end condition in your for loop'
+    const endsLate = 'Oops! The array includes the extra number 21...double check the end condition in your for loop';
+    const endsEarly = 'Oops! The array stops at 17 when it should include odd numbers up to 20...double check the end condition in your for loop';
 
 
     if(typeof katas1.oddsToTwenty === 'function') {
@@ -93,15 +93,15 @@ describe('Kata 3, Display an array with odd numbers 1 - 19', function() {
         
         it(studentAnswer.slice(-1)[0] === 21 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(21);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 17 ? endsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(17);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 4, Display an array with multiples of 5 from 5 - 100', function() {
 
@@ -109,7 +109,7 @@ describe('Kata 4, Display an array with multiples of 5 from 5 - 100', function()
         if(typeof katas1.multiplesOfFive != 'function') {
             this.skip();
         }
-    })
+    });
 
     const startsEarly = 'Oops! Array includes 0...double check the start condition in your for loop';
     const endsEarly = 'Oops! Array ends at 95...double check the end condition in your for loop';
@@ -121,15 +121,15 @@ describe('Kata 4, Display an array with multiples of 5 from 5 - 100', function()
 
         it(studentAnswer.slice(0)[0] === 0 ? startsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(0)[0]).to.not.eql(0);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 95 ? endsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(95);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 5, Display an array with all the square numbers up to 100', function() {
 
@@ -137,7 +137,7 @@ describe('Kata 5, Display an array with all the square numbers up to 100', funct
         if(typeof katas1.squareNumbers != 'function') {
             this.skip();
         }
-    })
+    });
 
     const endsLate = 'Oops! The array displays an extra square number out of range (121)...double check the end condition in your for loop';
     const startsEarly = 'Oops! The array displays 0. Double check the start condition in your for loop';
@@ -149,15 +149,15 @@ describe('Kata 5, Display an array with all the square numbers up to 100', funct
 
         it(studentAnswer.slice(-1)[0] === 121 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(121);
-        })
+        });
         it(studentAnswer.slice(0)[0] === 0 ? startsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(0)[0]).to.not.eql(0);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 6, Display an array with numbers counting backwards 20 - 1', function() {
 
@@ -165,7 +165,7 @@ describe('Kata 6, Display an array with numbers counting backwards 20 - 1', func
         if(typeof katas1.countingBackwards != 'function') {
             this.skip();
         }
-    })
+    });
     
     const startsEarly = 'Oops!  The array starts with 21 instead of 20...Double check the start condition in your for loop';
     const endsLate = 'Oops!  The array displays 0 instead of stopping at 1...Double check the end condition in your for loop';
@@ -179,18 +179,18 @@ describe('Kata 6, Display an array with numbers counting backwards 20 - 1', func
 
         it(JSON.stringify(studentAnswer) === JSON.stringify(notReversed) ? notReversedMessage : edgeSuccess, function() {
             expect(studentAnswer).to.not.eql(notReversed);
-        })
+        });
         it(studentAnswer.slice(0)[0] === 21 ? startsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(0)[0]).to.not.eql(21);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 0 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(0);
-        }) 
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 7, Display an array with even numbers counting backwards 20 - 2', function() {
 
@@ -198,7 +198,7 @@ describe('Kata 7, Display an array with even numbers counting backwards 20 - 2',
         if(typeof katas1.evenNumbersBackwards != 'function') {
             this.skip();
         }
-    })
+    });
 
     const correctAnswer = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2];
 
@@ -213,15 +213,15 @@ describe('Kata 7, Display an array with even numbers counting backwards 20 - 2',
 
         it(studentAnswer.slice(-1)[0] === 0 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(0);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(notReversed) ? notReversedMessage : edgeSuccess, function() {
             expect(studentAnswer).to.not.eql(notReversed);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 8, Display an array with odd numbers counting backwards 20 - 1', function() {
 
@@ -229,7 +229,7 @@ describe('Kata 8, Display an array with odd numbers counting backwards 20 - 1', 
         if(typeof katas1.oddNumbersBackwards != 'function') {
             this.skip();
         }
-    })
+    });
 
     const correctAnswer = [19, 17, 15, 13, 11, 9, 7, 5, 3, 1];
 
@@ -245,18 +245,18 @@ describe('Kata 8, Display an array with odd numbers counting backwards 20 - 1', 
 
         it(studentAnswer.slice(0)[0] === 21 ? startsEarly : edgeSuccess, function() {
             expect(studentAnswer.slice(0)[0]).to.not.eql(21);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === -1 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(-1);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(notReversed) ? notReversedMessage : edgeSuccess, function() {
             expect(studentAnswer).to.not.eql(notReversed);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer)
         })
     }
-})
+});
 
 describe('Kata 9, Display an array with multiples of 5 counting backwards from 100', function() {
 
@@ -264,7 +264,7 @@ describe('Kata 9, Display an array with multiples of 5 counting backwards from 1
         if(typeof katas1.multiplesOfFiveBackwards != 'function') {
             this.skip();
         }
-    })
+    });
 
     
     const correctAnswer = [100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5];
@@ -280,15 +280,15 @@ describe('Kata 9, Display an array with multiples of 5 counting backwards from 1
 
         it(JSON.stringify(studentAnswer) === JSON.stringify(notReversed) ? notReversedMessage : edgeSuccess, function() {
             expect(studentAnswer).to.not.eql(notReversed);
-        })
+        });
         it(studentAnswer.slice(-1)[0] === 0 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(0);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
 
 describe('Kata 10, Display an array with square numbers counting backwards from 100', function() {
 
@@ -296,7 +296,7 @@ describe('Kata 10, Display an array with square numbers counting backwards from 
         if(typeof katas1.squareNumbersBackwards != 'function') {
             this.skip();
         }
-    })
+    });
 
     const correctAnswer = [100, 81, 64, 49, 36, 25, 16, 9, 4, 1];
     const notReversed = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
@@ -310,12 +310,12 @@ describe('Kata 10, Display an array with square numbers counting backwards from 
 
         it(studentAnswer.slice(-1)[0] === 0 ? endsLate : edgeSuccess, function() {
             expect(studentAnswer.slice(-1)[0]).to.not.eql(0);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(notReversed) ? notReversedMessage : edgeSuccess, function() {
             expect(studentAnswer).to.not.eql(notReversed);
-        })
+        });
         it(JSON.stringify(studentAnswer) === JSON.stringify(correctAnswer) ? success : incorrect, function() {
             expect(studentAnswer).to.eql(correctAnswer);
         })
     }
-})
+});
