83bdf22ccd26d04238c9db906546874fae042da3
Ryan <jragard@gmail.com>
2019-03-15 11:47:49 -0400

    modified docstring for accuracy

--- index.js
+++ index.js
@@ -30,19 +30,18 @@ parser.addArgument(
     This script takes either a github/gitlab url as a 2nd argument (Ex: kenzie-test katas1 -g https://github.com/jragard/katas1) 
     or, to test a local file, 1 argument (Ex: kenzie-test katas1).  Git url args must have a -g or --gitlink flag.
 
-    On line 67, it begins by reading through the kenzie-testing/Tests directory. Then on line 75, for each test package
-    in this directory, it checks to see if the given assessmentArg (case insensitive) matches the test package. Once import PropTypes from 'prop-types'
-    finds a matching test package (for instance, Katas1), it sets 'validTestArgument' to 'true'.
+    It begins by reading through the kenzie-testing/Tests directory. Then for each test package
+    in this directory, it checks to see if the given assessment Arg (case insensitive) matches the test package.
 
-    If the user has supplied a valid assessment name (line 64), 'testLocation' is set to 'Tests/<testPackage>'
+    If the user has supplied a valid assessment name, 'testLocation' is set to 'Tests/<testPackage>'
     (Ex: 'Tests/Katas1'), and the script automatically navigates to that directory.
 
     Next, after switching to the appropriate ./Tests/ directory, it checks if the user is testing a local file, or if they
-    are testing a file via github/gitlab url.  If testing a local file, line 77 will read through the user's local directory, 
+    are testing a file via github/gitlab url.  If testing a local file, it will read through the user's local directory, 
     and for each of the local files in the users directory, we check if there is a valid local file to test.  (Valid local files must be named
     the same name as the test package directory, case insensitive (Ex: katas1 or Katas1). 
 
-    If a valid local file exists, line 87 will set 'userLocalFile' = to the valid file.  The script will then find the full path to
+    If a valid local file exists, it will set 'userLocalFile' = to the valid file.  The script will then find the full path to
     the valid file to be tested, reads the contents of the file and stores it in 'userLocalFileContent'.  It then creates a 'temp.js' file
     in the 'test' directory of the assessment in the npm package (Ex: Tests/Katas1/test), and writes 'userLocalFileContent' to the 'temp.js' file.
     Now we have a file identical the local file to be tested, in 'Tests/Katas1/test'.
