55c956d0cbb2e2c61a3a5baccb6daf2e2e35561f
Unknown <keybladeschoosen1@gmail.com>
2019-04-03 13:13:59 -0400

    Moved Anagrams 1 to only test via testcare

--- Tests/Anagrams1/run.js
+++ Tests/Anagrams1/run.js
@@ -3,14 +3,10 @@ const { spawn } = require("child_process");
 
 let test = new TestBase(
   __dirname,{
-  mochaFunctions:"getAnagramsOf",
-  mochaDom: "<!DOCTYPE html><html lang='en'><body><div><input type='text' id='input' size=40><button id='findButton'>Find Anagrams</button></div><script type='text/javascript' src='words.js'></script><script type='text/javascript' src='anagrams1.js'></script></body></html>",
-  mochaExtra: ["const { words } = require('./words.js');"],
     testCafeFixture: "Testing Anagrams 1"}
 );
 
 async function runTests() {
-    await test.runMochaTest();
   await test.runTestCafeTest();
 }
 
