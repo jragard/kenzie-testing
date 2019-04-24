484622dcfb9dc195c8717dccacba5a97ff15f29e
Unknown <keybladeschoosen1@gmail.com>
2019-04-24 11:38:33 -0400

    update to dotenv requirements

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-require('./node_modules/dotenv').config();
+require('dotenv').config({
+    path: `${__dirname}/.env`
+});
 const Parser = require('./lib/utils/Parser');
 const CommandHandler = require('./lib/utils/CommandHandler');
-
 let args = new Parser();
 let commandHandler = new CommandHandler(args);
 commandHandler.runCommand();
