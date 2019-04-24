024885c88d1fd51cd499222613f91ef875ed2f5d
Unknown <keybladeschoosen1@gmail.com>
2019-04-24 13:26:29 -0400

    Removing previous files

--- Tests/NumbersToWords/run.js
+++
@@ -1,9 +0,0 @@
-const { TestBase } = require("../../TestBase");
-
-let test = new TestBase(__dirname, { mochaFunctions: "numbersToWords" });
-
-async function runTests() {
-  await test.runMochaTest();
-}
-
-runTests();
